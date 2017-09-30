dbf8c022a087b022d3995ee3468c274ced036b5e
sago007 <poul@poulsander.com>
2017-09-30 16:59:07 +0200

    Added texture and license info

--- src/sago-map-generator-one.cpp
+++ src/sago-map-generator-one.cpp
@@ -1,3 +1,26 @@
+/*
+===========================================================================
+sago-map-generator-one
+Copyright (C) 2017 Poul Sander
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+
+Source information and contact persons can be found at
+https://github.com/sago007/sago-map-generator-one
+===========================================================================
+*/
+
 #include <iostream>
 #include <boost/program_options.hpp>
 #include <vector>
@@ -201,6 +224,7 @@ static theMap LayerMapToMap(const Config& c, const LayerMap& m) {
 		for (const Platform& p : m.layers.at(i).platforms) {
 			const int& u = c.unitSize;
 			Brush b = createBrush(u*p.x, u*p.y, u*i*(c.layerDistance),u*(p.x+p.w),u*(p.y+p.h),u*(i*(c.layerDistance)-c.platformThickness));
+			brushAddTexture(b, c.texture);
 			ret.brushes.push_back(b);
 		}
 	}
